55049510f4565a7570b6b4089172b75db7c146f3
bandofpv <andobernas@gmail.com>
2018-09-04 16:29:55 -0700

    Update MyName.java

--- src/MyName.java
+++ src/MyName.java
@@ -1,7 +1,7 @@
-public class MyName
-{
-  public static void main(String[] args)
-  {
+public class MyName{
+  
+  public static void main(String[] args){
+    
    System.out.println("    A            N      N  DD     RR     EEEE   W     W      W");
    System.out.println("  A  A        N N   N   D   D  R   R  E_ _ _    W  WW   W");
    System.out.println(" AAAA     N   N N    D   D  RR     E‾ ‾ ‾     WW   WW");
