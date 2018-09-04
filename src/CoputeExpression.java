e4a2852d164f92c130e8fe77f7e9423a082935e6
bandofpv <andobernas@gmail.com>
2018-09-04 09:48:57 -0700

    add questions

--- src/CoputeExpression.java
+++ src/CoputeExpression.java
@@ -3,7 +3,8 @@ public class CoputeExpression {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+		// Print out the result of (10.5 + 2 * 3) / (45 - 3.5)
+		System.out.println((10.5 + 2 * 3) / (45 - 3.5)); 
 	}
 
 }
