3f449c621000c49c633fe643973a1394a884efcf
bandofpv <andobernas@gmail.com>
2018-09-06 09:18:26 -0700

    Added more Java Code!

--- src/MyName.java
+++ src/MyName.java
@@ -2,10 +2,10 @@ public class MyName{
   
   public static void main(String[] args){
     // TODO Auto-generated method stub
-    System.out.println("    A            N      N  DD     RR     EEEE   W     W      W");
-    System.out.println("  A  A        N N   N   D   D  R   R  E_ _ _    W  WW   W");
-    System.out.println(" AAAA     N   N N    D   D  RR     E‾ ‾ ‾     WW   WW");
-    System.out.println("A       A  N      N      DD     R   R  EEEE        W      W");
+    System.out.println("   A       N    N  DD   RR   EEEE  W    W     W");
+    System.out.println("  A A     N N  N   D D  R R  E_ _   W   WW   W");
+    System.out.println(" AAAA    N   N N   D D  RR   E‾ ‾    WW    WW");
+    System.out.println("A    A  N      N   DD   R R  EEEE     W    W");
   }
   
 }
---
+++ src/WelcomeInMessageDialogBox.java
@@ -0,0 +1,15 @@
+/*  This applicatoin program displays Welcome to Java!
+ *  in a message dialog box
+ */
+
+import javax.swing.JOptionPane;
+
+public class WelcomeInMessageDialogBox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// display Welcome to Java! in a message dialog box
+		JOptionPane.showMessageDialog(null, "Welcome to Java!");
+	}
+
+}
