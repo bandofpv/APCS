bbe43d67b6f14c170e19652d57a4d2ddf26a2feb
bandofpv <andobernas@gmail.com>
2018-09-06 08:26:18 -0700

    Made java folder

---
+++ Java/src/CoputeExpression.java
@@ -0,0 +1,10 @@
+
+public class CoputeExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Print out the result of (10.5 + 2 * 3) / (45 - 3.5)
+		System.out.println((10.5 + 2 * 3) / (45 - 3.5)); 
+	}
+
+}
---
+++ Java/src/MyName.java
@@ -0,0 +1,11 @@
+public class MyName{
+  
+  public static void main(String[] args){
+    // TODO Auto-generated method stub
+    System.out.println("    A            N      N  DD     RR     EEEE   W     W      W");
+    System.out.println("  A  A        N N   N   D   D  R   R  E_ _ _    W  WW   W");
+    System.out.println(" AAAA     N   N N    D   D  RR     E‾ ‾ ‾     WW   WW");
+    System.out.println("A       A  N      N      DD     R   R  EEEE        W      W");
+  }
+  
+}
