f993ab76fde501946d676154b033e456f455d023
Bando FPV <andobernas@gmail.com>
2018-09-12 09:51:01 -0700

    add new code

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("scissor (0), rock (1), paper (2): ");
+		int user = input.nextInt();
+		
+		int comp = 
+	}
+
+}
