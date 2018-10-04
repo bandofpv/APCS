f60ea32a13a59c0d5d7d5da66e3a0933d99de4b7
Bando FPV <andobernas@gmail.com>
2018-10-04 09:29:08 -0700

    Finished all week 8 code

--- src/TestTV.java
+++ src/TestTV.java
@@ -10,7 +10,12 @@ public class TestTV {
 		
 		TV tv2 = new TV();
 		tv2.turnOn();
+		tv2.channelUp();
+		tv2.channelUp();
+		tv2.volumeUp();
 		
+		System.out.println("tv1's channel is " + tv1.channel + " and volume level is " + tv1.volumeLevel);
+		System.out.println("tv2's channel is " + tv2.channel + " and volume level is " + tv2.volumeLevel);
 	}
 
 }
