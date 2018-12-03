160cf30c40d90a15727a4d1ee08a0566df0e870d
bandofpv <andobernas@gmail.com>
2018-12-03 09:50:18 -0800

    updated convex hull

--- Quarter_2/Convex_Hull.java
+++ Quarter_2/Convex_Hull.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JApplet;
 import java.util.Vector;
+import java.awt.*;
+import javax.swing.*;
 
 class Point 
 { 
@@ -14,7 +16,15 @@ class Point
 // ADD TO POINT CLASS!!!!
 } 
   
-class Hull { 
+public class Convex_Hull extends JPanel { 
+	
+	private final int X_GRID_OFFSET = 25; // 25 pixels from left
+	private final int Y_GRID_OFFSET = 40; // 40 pixels from top
+	private final int CELL_WIDTH = 5;
+	private final int CELL_HEIGHT = 5;
+	
+	public static String xs = "";
+	public static String ys = "";
       
     // To find orientation of ordered triplet (p, q, r). 
     // The function returns following values 
@@ -23,6 +33,7 @@ class Hull {
     // 2 --> Counterclockwise 
     public static int orientation(Point p, Point q, Point r) 
     { 
+    	
         int val = (q.y - p.y) * (r.x - q.x) - 
                   (q.x - p.x) * (r.y - q.y); 
        
@@ -33,11 +44,9 @@ class Hull {
     // Prints convex hull of a set of n points. 
     public static void convexHull(Point points[], int n) 
     { 
+    	
     	int[] x = new int[7];
     	int[] y = new int[7];
-    	
-        // There must be at least 3 points 
-        if (n < 3) return; 
        
         // Initialize Result 
         Vector<Point> hull = new Vector<Point>(); 
@@ -82,76 +91,73 @@ class Hull {
        
         } while (p != l);  // While we don't come to first  
                            // point 
-       
-        // Print Result 
-        for (Point temp : hull) 
 
-            //System.out.println(temp.x + " " + temp.y); 
-            System.out.println(temp.x);
-        
-        System.out.println();
         
         // Print Result 
         for (Point temp : hull) 
 
-            //System.out.println(temp.x + " " + temp.y); 
-            System.out.println(temp.y);
-            
-    } 
-    
-    public static void main(String[] args)  
-    { 
-  
-        Point points[] = new Point[7]; 
-        points[0]=new Point(0, 3); 
-        points[1]=new Point(2, 3); 
-        points[2]=new Point(1, 1); 
-        points[3]=new Point(2, 1); 
-        points[4]=new Point(3, 0); 
-        points[5]=new Point(0, 0); 
-        points[6]=new Point(3, 3); 
-          
-        int n = points.length; 
-        convexHull(points, n); 
+        	xs += Integer.toString(temp.x);
         
-    
+        // Print Result 
+        for (Point temp : hull) 
+        	ys += Integer.toString(temp.y);
         
     }
-  
-}
+    
+    public static void main(String[] args) {
+		Point points[] = new Point[9]; 
+	    points[0]=new Point(5, 5); 
+	    points[1]=new Point(10, -10); 
+	    points[2]=new Point(-5, -5); 
+	    points[3]=new Point(10, 10); 
+	    points[4]=new Point(7, -7); 
+	    points[5]=new Point(-10, 10); 
+	    points[6]=new Point(-3, 3); 
+	    points[7]=new Point(-10, -10);
+	    points[8]=new Point(10, -10);
+	    
 
-public class Convex_Hull extends JApplet{
+	    
+	    int n = points.length; 
+	        
+	    convexHull(points, n);
+	    
+	    System.out.println(xs+ys);
+	    
+	    
+	    JFrame.setDefaultLookAndFeelDecorated(true);
+	    JFrame frame = new JFrame("Simple Window");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setBackground(Color.white);
+	    frame.setSize(800, 700);
+	 
+	    Convex_Hull panel = new Convex_Hull();
+	 
+	    frame.add(panel);
+	 
+	    frame.setVisible(true);
+	  }
+    
+    public double
+  
+    public void paintComponent(Graphics g) {
+	 
+	     //  box side lines
+	     g.setColor(Color.black);
+	     g.drawLine((xs.charAt(0) - X_GRID_OFFSET - 1) / (CELL_WIDTH + 1),ys.charAt(0) + 120, xs.charAt(1) + 120, ys.charAt(1) + 20);
+	     g.drawLine(xs.charAt(1) + 120,ys.charAt(1) + 20, xs.charAt(2) + 20, ys.charAt(2) + 20);
+	     g.drawLine(xs.charAt(2) + 20,ys.charAt(2) + 20, xs.charAt(3) + 20, ys.charAt(3) + 120);
+	     g.drawLine(xs.charAt(3) + 20,ys.charAt(3) + 120, xs.charAt(0) + 120, ys.charAt(0) + 120);
+	     
+	     int x = (arg4.getX() - X_GRID_OFFSET - 1) / (CELL_WIDTH + 1);
+			int y = (arg4.getY() - Y_GRID_OFFSET - 4) / (CELL_HEIGHT + 1);
 	
-	public void init() {
-		
-		setSize(800, 800);
-		repaint();
-	}
-	public void paint(Graphics g) {
-		drawBackground(g);
-		drawPoints(g);
-	}
+	     //  text identifiers
+	     g.drawString("10,-10",120,120);
+	     g.drawString("10,10", 120, 20);
+	     g.drawString("-10,10", 20, 20);
+	     g.drawString("-10,10", 20,120);
+	  }
 	
-	private void drawBackground(Graphics g) {
-		
-		g.setColor(Color.WHITE);
-		g.fillRect(0,0, (int)(getWidth()), (int)(getHeight()));
-		
-	}
-	public void drawPoints(Graphics g) {
-		
-		int off = 50;
-		int off2 = 300;
-		
-		g.setColor(Color.BLACK);
-		g.fillOval(400 + 0 * off,400 - 3 * off,10,10);
-		g.fillOval(400 + 0 * off,400 - 0 * off,10,10);
-		g.fillOval(400 + 3 * off,400 - 0 * off,10,10);
-		g.fillOval(400 + 3 * off,400 - 3 * off,10,10);
-		
-		
-		
-	}
-   
 
 } 
