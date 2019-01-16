2e889665c096fdd22f5ae36ea562f19f46940b39
bandofpv <andobernas@gmail.com>
2019-01-15 21:56:41 -0800

    Worked on Big.java

--- Quarter_3/Big.java
+++ Quarter_3/Big.java
@@ -3,7 +3,22 @@ public class Big {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int integer = 2147483647;
+	      System.out.println(Math.pow(integer + 1, 10));
+	      for (int i = 0; i < 30; i++){
+	         System.out.println(i + ": " + factorial(i));
+	      }
+		
 	}
+	
+	public static int factorial(int number){
+	      int result = 1;
+	      for (int i = number;i > 0;i--){
+	         result = result * i;
+	      }
+	      return result; 
+	   }
+
 
 }
