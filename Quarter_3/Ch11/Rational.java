5a97c239f4154bcd9b144fed36cd4da1a1eb7ff4
bandofpv <andobernas@gmail.com>
2019-01-28 09:52:00 -0800

    Finalized Rational code!!!

--- Quarter_3/Ch11/Rational.java
+++ Quarter_3/Ch11/Rational.java
@@ -3,30 +3,69 @@ package Ch11;
 public class Rational{
 	public static void main(String args[]){
 		Rational rat1 = new Rational();
-
+		
+		System.out.println("Using printRational method to print default rational");
 		rat1.printRational();
-		System.out.println(rat1.toString());
+		
+		System.out.println("");
+		
+		System.out.println("Using toString method to print default rational");
+		System.out.println(rat1);
+		
+		System.out.println("");
 
 		Rational rat2 = new Rational(1, 2);
 
+		System.out.println("Using toString method to print 1/2 rational");
 		System.out.println(rat2);
+		
+		System.out.println("");
+		
+		System.out.println("Negating 1/2 to -1/2");
 		rat2.negate();
 		System.out.println(rat2);
 
+		System.out.println("");
+		
+		System.out.println("Inverting -1/2 to -2/1");
 		rat2.invert();
 		System.out.println(rat2);
 
+		System.out.println("");
+		
+		System.out.println("Turning -2/1 into a double --> -2.0");
 		System.out.println(rat2.toDouble());
 
 		Rational rat3 = new Rational(5, 15);
 
+		System.out.println("");
+		
+		System.out.println("Reducing 5/15 to 1/3");
 		System.out.println(rat3.reduce());
 
 		Rational rat4 = new Rational(1, 4);
 
 		Rational rat5 = new Rational(1, 4);
 
+		System.out.println("");
+		
+		System.out.println("Adding 1/4 and 1/4 to get 1/2");
 		System.out.println(rat4.add(rat5));
+		
+		System.out.println("");
+		
+		Rational rat6 = new Rational(-1, 2);
+		rat6.invert();
+		System.out.println("Inverting -1/2 correctly");
+		System.out.println(rat6);
+		Rational rat7 = new Rational(-1, 2);
+		System.out.println("Inverting -1/2 incorrectly");
+		rat6.wrongInvert();
+		System.out.println(rat6);
+		
+		if (rat7.equals(rat6) == false) {
+			System.out.println("Test Failed!!!");
+		}
 	}
 
 	private int	numerator;
@@ -72,13 +111,15 @@ public class Rational{
 		this.extra();
 	}
 
-	//	public void	invert(){
-	//		
-	//		this.numerator = this.denominator;
-	//		this.denominator = this.numerator;
-	//		
-	//		this.extra();
-	//	}
+		public void	wrongInvert(){
+			
+			this.numerator = this.denominator;
+			this.denominator = this.numerator;
+			
+			this.extra();
+	
+		}
+
 
 	public Double toDouble(){
 
