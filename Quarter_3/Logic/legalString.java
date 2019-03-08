90b2d12ece930b5af7a7fb809bbbe88d094fabb5
Bando FPV <andobernas@gmail.com>
2019-03-08 09:49:48 -0800

    no U

--- Quarter_3/Logic/legalString.java
+++ Quarter_3/Logic/legalString.java
@@ -25,16 +25,18 @@ public class legalString {
 	}
 	private String str;
 
-
+	//Constructor assingin legalString object to str
 	public legalString(String str) {
 		this.str = str;
 	}
-
+	
+	//Checks if the sentence is simiple and legal
 	public static boolean simple(String str) {
 		if("~~".contains(str) != false)return false;
 		return"~a&&b~b~a||ba||~ba&&~ba<==>b~a<==>~b~a<==>ba<==>~b".contains(str);
 	}
-
+	
+	//check if sentence in parens is simple or complex and legal
 	public static boolean legal(String a) {
 		if(simple(a)) return true;
 
@@ -47,7 +49,8 @@ public class legalString {
 
 		return false;
 	}
-
+	
+	//toString method returning String of logical sentence or if it's not legal than "Sorry, thats not legal......."
 	public String toString() {
 		if(simple(this.str)) {
 			return this.str;
