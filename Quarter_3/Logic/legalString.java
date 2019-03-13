e397311a446e08585a480f563b6288f5d9a9fb15
Bando FPV <andobernas@gmail.com>
2019-03-13 09:45:42 -0700

    added evaluate method

--- Quarter_3/Logic/legalString.java
+++ Quarter_3/Logic/legalString.java
@@ -4,28 +4,44 @@ public class legalString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Cases: \n");
-
-		System.out.println("Check if \"a\" is legal");
-		System.out.println(legal("a") + "\n");
-
-		System.out.println("Check if \"a&&b\" is legal");
-		System.out.println(legal("a&&b")+ "\n");
-
-		System.out.println("Check if \"(a&&b)\" is legal");
-		System.out.println(legal("(a&&b)") + "\n");
-
-		System.out.println("Check if \"((a&&b) || c)\" is legal");
-		System.out.println(legal("((a&&b) || c)") + "\n");
+//		System.out.println("Test Cases: \n");
+//
+//		System.out.println("Check if \"a\" is legal");
+//		System.out.println(legal("a") + "\n");
+//
+//		System.out.println("Check if \"a&&b\" is legal");
+//		System.out.println(legal("a&&b")+ "\n");
+//
+//		System.out.println("Check if \"(a&&b)\" is legal");
+//		System.out.println(legal("(a&&b)") + "\n");
+//
+//		System.out.println("Check if \"((a&&b) || c)\" is legal");
+//		System.out.println(legal("((a&&b) || c)") + "\n");
+//		
+//		System.out.println("Edge Cases: \n");
+//		
+//		System.out.println("Test if \")(a)(\" is legal");
+//		System.out.println(legal(")(a)("));
 		
-		System.out.println("Edge Cases: \n");
+		String[] str = new String[1];
+		str[0] = "a||b";
+		TruthAssignment ta = new TruthAssignment(str);
+		System.out.println("Created a String of \"a||b\" and assigned it to false.");
+		System.out.println("evaluate method returns:");
+		System.out.println(evaluate(str[0], ta) + "\n");
 		
-		System.out.println("Test if \")(a)(\" is legal");
-		System.out.println(legal(")(a)("));
+		String[] str1 = new String[1];
+		str1[0] = "a&&b";
+		boolean[] bool1 = new boolean[1];
+		bool1[0] = true;
+		TruthAssignment ta1 = new TruthAssignment(str1, bool1);
+		System.out.println("Created a String of \"a&&b\" and assigned it to true.");
+		System.out.println("evaluate method returns:");
+		System.out.println(evaluate(str1[0], ta1) + "\n");
 	}
 	private String str;
 
-	//Constructor assingin legalString object to str
+	//Constructor assigning legalString object to str
 	public legalString(String str) {
 		this.str = str;
 	}
@@ -42,12 +58,23 @@ public class legalString {
 
 		else if(a.contains("(") && a.contains(")")) {
 			if(a.lastIndexOf('(') < a.indexOf(')')) {
-				return simple(a.substring(a.lastIndexOf('(') + 1, a.indexOf(')')));
+				return simple(a.substring(a.lastIndexOf('('
+						) + 1, a.indexOf(')')));
 			}
 			return false;
 		}
 
 		return false;
+	}
+	
+	//method that takes a string representing a logical sentence and a TruthAssignment which returns a boolean that is the result of evaluating the logical sentence with the given truth assignment
+	public static boolean evaluate(String str, TruthAssignment ta) {
+		if (legal(str)) return ta.getTruthProp(str);
+		return false;
+	}
+	
+	public static void generate() {
+		
 	}
 	
 	//toString method returning String of logical sentence or if it's not legal than "Sorry, thats not legal......."
